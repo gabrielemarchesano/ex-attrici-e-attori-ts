1a9c32ce0e8a57c53662fcfd50fcb5ac19cc3fb8
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-21 18:35:30 +0200

    milestone 2: create type alias Actress that contains Person properties plus its own properties

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,10 @@ type Person = {
   death_year?: number,
   biography: string,
   image: string
+};
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: "American"| "British"| "Australian"| "Italo-American"| "South African"| "French"| "Indian"| "German"| "Spanish"| "South Korean"| "Chinese"
 }
